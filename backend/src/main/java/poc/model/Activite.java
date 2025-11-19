bb80e14f99775b9db86b09e833c261e55db9b2b5
powarth <148247288+powarth@users.noreply.github.com>
2025-11-19 17:46:53 +0100

    Create Activite.java

---
+++ backend/src/main/java/poc/model/Activite.java
@@ -0,0 +1,18 @@
+package poc.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "activite")
+public class Activite {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titre;
+
+    @Enumerated(EnumType.STRING)
+    private TypeActivite type;
+}
